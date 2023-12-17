064d8d0f98775c9d648cb31836c9267729b1291b
Milen Minchev <milennelim90@gmail.com>
2023-12-17 01:55:20 +0000

    a

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
       },
       includeAssets: ['logo.png'],
       manifest: {
-        name: 'Portfolioo',
+        name: 'Portfolio',
         short_name: 'Portfolio',
         description: 'Personal Portfolio',
         icons: [
